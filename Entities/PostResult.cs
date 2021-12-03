1be925e824d64a8f6ca0b143c9a957b25bde3275
András Tóth <andrastoth1126@gmail.com>
2021-12-03 16:38:27 +0100

    Initial commit

---
+++ Entities/PostResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pingbacker.Entities
+{
+    public class PostResult
+    {
+        public List<string> Categories { get; set; }
+
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        public Guid SourceId { get; set; }
+    }
+}
---
+++ Entities/RssSource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pingbacker.Entities
+{
+    public class RssSource
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Name { get; set; }
+
+        public string RssUrl { get; set; }
+    }
+}
